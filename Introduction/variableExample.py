187f0855c49e2fbb05ca930589a6757495b3250b
Nishanshah54 <Nessanshahi54@gmail.com>
2024-06-25 11:57:10 +0545

    Variables and Datatypes

---
+++ Introduction/variableExample.py
@@ -0,0 +1,52 @@
+x = 10      # x is an integer
+print(x)
+x = "ten"   # x is now a string
+print(x)
+
+x = 5
+name = "John"
+is_active = True
+print (is_active)
+print(type(x))
+print(type(name))
+
+### Multiple Assignments
+a, b, c = 1, 2, 3
+print (a)
+print (b)
+print (c)
+# integers
+age=20
+# Floating points
+height=5.6
+# String 
+namem ="Hari"
+# Booleans
+is_student=True
+# # lists
+Fruits=["apple","banana","cherry","mango","jackfruits"]
+print(Fruits)
+# tuple
+# coordinates=(10.0,20.0)
+
+#  **Dictionaries**
+student = {"name": "John", "age": 20}
+print (student)
+### Changing Variable Types
+# You can change the type of a variable by assigning a value of a different type:
+
+x = 10      # x is an integer
+x = "ten"   # x is now a string
+#  Printing variable values
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is student:", is_student)
+
+# Changing variable values
+age = 31
+is_student = True
+
+# Printing updated values
+print("Updated Age:", age)
+print("Updated Is student:", is_student)
